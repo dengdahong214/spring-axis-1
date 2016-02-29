c00cc9f2f8d1ccc5fa2a4aa1e1b5b41290b69fae
Pawel Korus <pawelkorus@ymail.com>
2016-02-29 13:46:18 +0100

    axis service integrated with spring context

--- spring-axis-1/src/main/java/local/spring/axis/service/HelloWorldService.java
+++ spring-axis-1/src/main/java/local/spring/axis/service/HelloWorldService.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package local.spring.axis.service;
+
+import org.springframework.stereotype.Component;
 
 /**
  *
  * @author plks
  */
+@Component
 public class HelloWorldService {
     public String helloWorld() {
         return "Hello World.";
