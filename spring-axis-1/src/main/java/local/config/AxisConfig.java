6468367af9f71d4eb17641e7e294fc6f917fbd86
Pawel Korus <pawelkorus@ymail.com>
2016-03-10 15:19:52 +0100

    overriding EngineConfigurationFactory so that server-config.wsdd is loaded as a resource by class loader without throwing any errors.

--- spring-axis-1/src/main/java/local/config/AxisConfig.java
+++ spring-axis-1/src/main/java/local/config/AxisConfig.java
@@ -5,6 +5,7 @@
  */
 package local.config;
 
+import org.apache.axis.EngineConfigurationFactory;
 import org.apache.axis.transport.http.AdminServlet;
 import org.apache.axis.transport.http.AxisServlet;
 import org.springframework.boot.context.embedded.ServletRegistrationBean;
@@ -20,6 +21,7 @@ public class AxisConfig {
     
     @Bean
     public ServletRegistrationBean axisServletRegistrationBean() {
+        System.setProperty(EngineConfigurationFactory.SYSTEM_PROPERTY_NAME, local.axis.EngineConfigurationFactory.class.getName());
         AxisServlet servlet = new AxisServlet();
         ServletRegistrationBean servletBean = new ServletRegistrationBean(servlet, "/axis/*", "/axis-secured/*");
         return servletBean;
